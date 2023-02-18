4f3adaea520d8f043cf3b216bfbc2aeb298a7ed0
Antonio Vinicius <antvinicius@gmail.com>
2023-02-18 17:52:42 -0300

    oitavo commit

--- controllers/site.usuarioadmin.controller.js
+++ controllers/site.usuarioadmin.controller.js
@@ -4,7 +4,7 @@ async function renderUsuarioadmin(req, res, _next){
     res.render('site/usuarioadmin', Object.assign({}, {
         title: 'Administração do Usuário - Hotel Ventura',
         header: {
-          background: 'images/img_bg_3.jpg',
+          background: 'images/img_bg_6.jpg',
           title: 'Administração do Usuário'
         },
         headerIndex: false,
